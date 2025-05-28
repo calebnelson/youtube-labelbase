271d5a867259b594fff075f737a6349407272f9e
Caleb Nelson <calebnelson@gmail.com>
2025-05-27 17:52:09 -0700

    change install command

--- backend/generate_requirements.sh
+++ backend/generate_requirements.sh
@@ -9,4 +9,14 @@ sed -i '' '/^black/d' requirements.txt
 sed -i '' '/^isort/d' requirements.txt
 sed -i '' '/^flake8/d' requirements.txt
 
-echo "Generated requirements.txt from Pipfile" 
+# Remove version constraints and keep only the package names
+sed -i '' 's/;.*$//' requirements.txt
+sed -i '' 's/==.*$//' requirements.txt
+
+# Remove the -i line
+sed -i '' '/^-i/d' requirements.txt
+
+# Remove empty lines
+sed -i '' '/^$/d' requirements.txt
+
+echo "Generated simplified requirements.txt from Pipfile" 
