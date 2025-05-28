9f01d284e8d0235e6e050484a8b61d9f95a59df2
Caleb Nelson <calebnelson@gmail.com>
2025-05-27 17:35:10 -0700

    trying to deploy from root

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
+    allow_origins=settings.CORS_ORIGINS + ["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
