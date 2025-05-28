20354abb52c0bc97be28cc8bc5dc60a48c9af5b2
Caleb Nelson <calebnelson@gmail.com>
2025-05-28 13:59:49 -0700

    add pythonpath

--- backend/generate_requirements.sh
+++ backend/generate_requirements.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Generate requirements.txt from Pipfile into app directory
-pipenv requirements > app/requirements.txt
+pipenv requirements > requirements.txt
 
 # Remove any dev dependencies
-sed -i '' '/^pytest/d' app/requirements.txt
-sed -i '' '/^black/d' app/requirements.txt
-sed -i '' '/^isort/d' app/requirements.txt
-sed -i '' '/^flake8/d' app/requirements.txt
+sed -i '' '/^pytest/d' requirements.txt
+sed -i '' '/^black/d' requirements.txt
+sed -i '' '/^isort/d' requirements.txt
+sed -i '' '/^flake8/d' requirements.txt
 
 # Remove version constraints and keep only the package names
-sed -i '' 's/;.*$//' app/requirements.txt
-sed -i '' 's/==.*$//' app/requirements.txt
+sed -i '' 's/;.*$//' requirements.txt
+sed -i '' 's/==.*$//' requirements.txt
 
 # Remove the -i line
-sed -i '' '/^-i/d' app/requirements.txt
+sed -i '' '/^-i/d' requirements.txt
 
 # Remove empty lines
-sed -i '' '/^$/d' app/requirements.txt
+sed -i '' '/^$/d' requirements.txt
 
 echo "Generated simplified requirements.txt in app directory from Pipfile" 
